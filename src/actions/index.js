3c474e84837fbc819cebc938f125a52c0f32df19
Evan Guirino <etg624@gmail.com>
2019-03-08 09:14:56 -0800

    export all cat/dog actions

---
+++ src/actions/index.js
@@ -0,0 +1,39 @@
+import {
+  FETCH_DOG_ERROR,
+  FETCH_DOG_REQUEST,
+  FETCH_DOG_SUCCESS,
+  fetchDog,
+  fetchDogError,
+  fetchDogRequest,
+  fetchDogSuccess,
+  adoptDog
+} from './dog';
+import {
+  FETCH_CAT_ERROR,
+  FETCH_CAT_REQUEST,
+  FETCH_CAT_SUCCESS,
+  fetchCat,
+  fetchCatError,
+  fetchCatRequest,
+  fetchCatSuccess,
+  adoptCat
+} from './cat';
+
+export default {
+  FETCH_CAT_ERROR,
+  FETCH_CAT_REQUEST,
+  FETCH_CAT_SUCCESS,
+  fetchCat,
+  fetchCatError,
+  fetchCatRequest,
+  fetchCatSuccess,
+  FETCH_DOG_ERROR,
+  FETCH_DOG_REQUEST,
+  FETCH_DOG_SUCCESS,
+  fetchDog,
+  fetchDogError,
+  fetchDogRequest,
+  fetchDogSuccess,
+  adoptCat,
+  adoptDog
+};
