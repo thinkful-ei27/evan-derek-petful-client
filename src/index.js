bc12a9ca510a34cc4d8c5ee411e3ef3bfbf3186c
Evan Guirino <etg624@gmail.com>
2019-03-08 08:33:23 -0800

    boiler plate client

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Dashboard from './Dashboard';
 import * as serviceWorker from './serviceWorker';
+const dog = {
+  imageURL:
+    'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription:
+    'A smiling golden-brown golden retreiver listening to music.',
+  name: 'Zeus',
+  sex: 'Male',
+  age: 3,
+  breed: 'Golden Retriever',
+  story: 'Owner Passed away'
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+const cat = {
+  imageURL:
+    'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+  imageDescription:
+    'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+};
+ReactDOM.render(
+  <Dashboard catToAdopt={cat} dogToAdopt={dog} />,
+  document.getElementById('root')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
